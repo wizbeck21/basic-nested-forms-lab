55fe04898c54c9e7344c8e8587c86ab3e253e978
Wesley <wes.beck21@outlook.com>
2021-01-21 17:38:53 -0800

    commit tree 73fece0fac29f28566a3ff9a8c514fd323502b1f

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients 
+  accepts_nested_attributes_for :ingredients
+
+  # def ingredients_attributes=(ingredients_attributes)
+  #   ingredients_attributes.each do |ingredient_attributes|
+  #     self.ingredients.build(ingredient_attributes)
+  #   end
+  # end
 end
